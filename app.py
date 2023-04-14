4c739a5cb41ad562ad1e58440a6bbbb47a7f7bb0
Siddhant <104164130+siddhant-shekhar@users.noreply.github.com>
2023-04-14 10:42:28 +0000

    connected db

--- app.py
+++ app.py
@@ -1,32 +1,13 @@
 from flask import Flask, render_template
+from database import load_jobs_from_db
 
 app = Flask(__name__)
-
-JOBS = [{
-  'id': 1,
-  'title': 'Data Analyst',
-  'location': 'Bengaluru, India',
-  'salary': 'Rs. 10,00,000'
-}, {
-  'id': 2,
-  'title': 'Data Scientist',
-  'location': 'Delhi, India',
-  'salary': 'Rs. 15,00,000'
-}, {
-  'id': 3,
-  'title': 'Frontend Engineer',
-  'location': 'Remote'
-}, {
-  'id': 4,
-  'title': 'Backend Engineer',
-  'location': 'San Francisco, USA',
-  'salary': '$150,000'
-}]
-
+  
 
 @app.route("/")
 def main():
-  return render_template('home.html', jobs=JOBS)
+  jobs= load_jobs_from_db()
+  return render_template('home.html', jobs=jobs)
 
 
 @app.route("/not_ready")
