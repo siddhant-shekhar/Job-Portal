298bf1fe4a8bc943a97e0b3a16b8bfc2066754f0
Siddhant <104164130+siddhant-shekhar@users.noreply.github.com>
2023-04-14 14:32:59 +0000

    added dynamic pages for each job

--- app.py
+++ app.py
@@ -18,8 +18,17 @@ def not_ready_page():
 @app.route("/job/<id>")
 def show_job(id):
   job = load_job_from_db(id)
-  return jsonify(job)
 
+  if not job:
+    return "Not Found", 404
+
+  return render_template('jobpage.html', job=job)
+
+
+# @app.route("/job/<id>")
+# def show_job_json(id):
+#   job = load_job_from_db(id)
+#   return jsonify(job)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -22,11 +22,9 @@ def load_jobs_from_db():
 
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(text(f"SELECT * FROM jobs WHERE id={id}"))
-    rows = []
-    for row in result.all():
-      rows.append(row._mapping)
+    result = conn.execute(text(f"SELECT * FROM jobs WHERE id = {id}"), )
+    rows = result.all()
     if len(rows) == 0:
       return None
     else:
-      return [dict(row) for row in rows]
+      return rows[0]._mapping
